854c0d9e8d15fc188fc721e646e07b3d9d91edcd
Filip Niksic <fniksic@gmail.com>
2020-01-20 20:24:09 -0500

    Set up dependencies and logging; add the main class

---
+++ src/main/java/edu/upenn/zktester/ZKTester.java
@@ -0,0 +1,13 @@
+package edu.upenn.zktester;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ZKTester {
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    public static void main(final String[] args) {
+        LOG.info("Hello!");
+    }
+}
