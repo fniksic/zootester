75641948183ca04e225145e9864173eae3e6b0b9
Filip Niksic <fniksic@gmail.com>
2020-01-28 00:31:10 -0500

    Parallelize execution

--- src/main/java/edu/upenn/zktester/ZKTester.java
+++ src/main/java/edu/upenn/zktester/ZKTester.java
@@ -2,24 +2,38 @@ package edu.upenn.zktester;
 
 import edu.upenn.zktester.scenario.RandomScenario;
 import edu.upenn.zktester.scenario.Scenario;
-import edu.upenn.zktester.util.AssertionFailureError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZKTester {
 
     private static final Logger LOG = LoggerFactory.getLogger(ZKTester.class);
 
     public static void main(final String[] args) {
-        final Scenario scenario = new RandomScenario(1000, 3, 1, 2);
 //        final Scenario scenario = new DivergenceResyncScenario();
-        try {
-            scenario.init();
-            scenario.execute();
-        } catch (final Exception e) {
-            LOG.error("Exception while executing scenario", e);
-        } catch (final AssertionFailureError e) {
-            LOG.error("Assertion failed", e);
+        final List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 16; ++i) {
+            final Thread thread = new Thread(() -> {
+                try {
+                    final Scenario scenario = new RandomScenario(1000, 3, 1, 2);
+                    scenario.init();
+                    scenario.execute();
+                } catch (final Exception e) {
+                    LOG.error("Exception while executing scenario", e);
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+        for (final var thread : threads) {
+            try {
+                thread.join();
+            } catch (final InterruptedException e) {
+                LOG.info("Interrupted from waiting on {}", thread.getName());
+            }
         }
     }
 }
