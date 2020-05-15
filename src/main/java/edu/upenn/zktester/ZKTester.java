54b91bba382a2772cc6011a28e68b1849a817f49
Filip Niksic <fniksic@seas.upenn.edu>
2020-05-15 09:20:57 -0400

    Fix running the interesting scenario in parallel

--- src/main/java/edu/upenn/zktester/ZKTester.java
+++ src/main/java/edu/upenn/zktester/ZKTester.java
@@ -34,9 +34,6 @@ public class ZKTester {
                 case "baseline":
                     scenario = new ParallelBaselineScenario();
                     break;
-                case "interesting":
-                    scenario = new InterestingScenario();
-                    break;
                 default:
                     LOG.error("Unknown scenario!");
                     throw new Exception("Unknown scenario");
