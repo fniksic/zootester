1efa1bab7d9d04d1e512cd4025207c22917f1aba
Filip Niksic <fniksic@seas.upenn.edu>
2020-05-16 02:34:48 -0400

    Comment out 6 scenarios in ParallelBaselineHarnessLong

--- src/main/java/edu/upenn/zktester/scenario/ParallelBaselineHarnessLong.java
+++ src/main/java/edu/upenn/zktester/scenario/ParallelBaselineHarnessLong.java
@@ -39,13 +39,14 @@ public class ParallelBaselineHarnessLong extends ParallelBaselineScenario {
                         new UnconditionalWritePhase(1, "/key1", 401),
                         new EmptyPhase()
                 )),
-                new Harness(List.of(
-                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 101),
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(0, "/key1", 101, "/key0", 200),
-                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 301),
-                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 400)
-                )),
+                //Interesting one
+//                new Harness(List.of(
+//                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 101),
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(0, "/key1", 101, "/key0", 200),
+//                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 301),
+//                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 400)
+//                )),
                 new Harness(List.of(
                         new EmptyPhase(),
                         new UnconditionalWritePhase(2, "/key1", 102),
@@ -59,42 +60,42 @@ public class ParallelBaselineHarnessLong extends ParallelBaselineScenario {
                         new ConditionalWritePhase(1, "/key1", 0, "/key0", 301),
                         new EmptyPhase(),
                         new ConditionalWritePhase(2, "/key0", 0, "/key1", 402)
-                )),
-                new Harness(List.of(
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(2, "/key1", 0, "/key1", 102),
-                        new ConditionalWritePhase(1, "/key0", 0, "/key1", 201),
-                        new UnconditionalWritePhase(1, "/key0", 301),
-                        new ConditionalWritePhase(2, "/key0", 301, "/key0", 402)
-                )),
-                new Harness(List.of(
-                        new UnconditionalWritePhase(2, "/key0", 102),
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(1, "/key0", 102, "/key0", 201),
-                        new UnconditionalWritePhase(1, "/key0", 301),
-                        new ConditionalWritePhase(1, "/key0", 102, "/key0", 401)
-                )),
-                new Harness(List.of(
-                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 100),
-                        new ConditionalWritePhase(2, "/key0", 100, "/key0", 202),
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 301),
-                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 400)
-                )),
-                new Harness(List.of(
-                        new ConditionalWritePhase(2, "/key1", 0, "/key1", 102),
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(2, "/key1", 102, "/key1", 202),
-                        new ConditionalWritePhase(0, "/key1", 202, "/key1", 300),
-                        new ConditionalWritePhase(2, "/key1", 0, "/key0", 402)
-                )),
-                new Harness(List.of(
-                        new ConditionalWritePhase(1, "/key0", 0, "/key0", 101),
-                        new ConditionalWritePhase(0, "/key0", 101, "/key0", 200),
-                        new ConditionalWritePhase(1, "/key0", 101, "/key0", 301),
-                        new EmptyPhase(),
-                        new ConditionalWritePhase(1, "/key0", 101, "/key1", 401)
                 ))
+//                new Harness(List.of(
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(2, "/key1", 0, "/key1", 102),
+//                        new ConditionalWritePhase(1, "/key0", 0, "/key1", 201),
+//                        new UnconditionalWritePhase(1, "/key0", 301),
+//                        new ConditionalWritePhase(2, "/key0", 301, "/key0", 402)
+//                )),
+//                new Harness(List.of(
+//                        new UnconditionalWritePhase(2, "/key0", 102),
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(1, "/key0", 102, "/key0", 201),
+//                        new UnconditionalWritePhase(1, "/key0", 301),
+//                        new ConditionalWritePhase(1, "/key0", 102, "/key0", 401)
+//                )),
+//                new Harness(List.of(
+//                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 100),
+//                        new ConditionalWritePhase(2, "/key0", 100, "/key0", 202),
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(1, "/key1", 0, "/key1", 301),
+//                        new ConditionalWritePhase(0, "/key1", 0, "/key0", 400)
+//                )),
+//                new Harness(List.of(
+//                        new ConditionalWritePhase(2, "/key1", 0, "/key1", 102),
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(2, "/key1", 102, "/key1", 202),
+//                        new ConditionalWritePhase(0, "/key1", 202, "/key1", 300),
+//                        new ConditionalWritePhase(2, "/key1", 0, "/key0", 402)
+//                )),
+//                new Harness(List.of(
+//                        new ConditionalWritePhase(1, "/key0", 0, "/key0", 101),
+//                        new ConditionalWritePhase(0, "/key0", 101, "/key0", 200),
+//                        new ConditionalWritePhase(1, "/key0", 101, "/key0", 301),
+//                        new EmptyPhase(),
+//                        new ConditionalWritePhase(1, "/key0", 101, "/key1", 401)
+//                ))
         ));
     }
 }
